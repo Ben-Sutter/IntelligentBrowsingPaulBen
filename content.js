e0a93a0c18ad9ef204743ff180234636f6a78e5e
Paul Kim <pauldk422@gmail.com>
2021-12-07 21:15:10 -0600

    Update content.js

--- content.js
+++ content.js
@@ -5,17 +5,55 @@ function clear() {
     for(const snippet of contents){
         snippet.style.backgroundColor = "initial";
     }
+
 }
 
 function findText(find, results){
+    find = find.toLowerCase();
+    chrome.storage.local.set({output:"undefined"});
     // const regex = new RegExp(find, "gi");
-    clear();
-    const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
+    const key = "?key=fb9962e8-8321-4ec5-8b85-d23c0bf80d5b";
+    const url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" + find + key;
+    console.log(url);
+    fetch(url)
+    .then(response =>{
+      if (!response.ok) {
+        console.log("error");
+      }
+      return response.json();
+    })
+    .then(data =>{
+      var syns = [];
+      syns.push(find);
+      var flag = true;
+      if (!data[0].meta) {
+        console.log("not a real word");
+        flag = false;
+      }
+      if(flag) {
+        for (var i =0; i < data.length; i++) {
+          if (data[i].meta.id.normalize() === find.normalize()) {
+            for (var j = 0; j < data[i].meta.syns.length; j++) {
+              syns = syns.concat(data[i].meta.syns[j])
+            }
+          }
+        }
+      }
+      console.log(syns);
+      helper(syns);
+    });
 
-    console.log("in script");
 
-    outputArray = [];
+}
 
+function helper(syns) {
+  clear();
+  const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
+
+  console.log("in script");
+
+  outputArray = [];
+  for (const find of syns) {
     for(const snippet of contents){
         // snippet.textContent = snippet.textContent.replace(regex, replace);
         if(snippet.textContent.toLowerCase().includes(find.toLowerCase())){
@@ -29,19 +67,21 @@ function findText(find, results){
             }
         }
     }
-    outputText = "";
-    for(elem of outputArray){
-        outputText = outputText.concat(elem);
-        outputText = outputText.concat(" \n ");
-        outputText = outputText.concat(" \n ");
-    }
-    console.log(outputText);
-    // results.insertAdjacentHTML('afterbegin', outputText);
-
+  }
+  outputText = "";
+  for(elem of outputArray){
+      outputText = outputText.concat("<li>");
+      outputText = outputText.concat(elem);
+      outputText = outputText.concat("</li>");
+  }
+  console.log(outputText);
+  chrome.storage.local.set({output:outputText});
+  // results.insertAdjacentHTML('afterbegin', outputText);
 }
 
 chrome.runtime.onMessage.addListener(function(message){
     if(message.action === 'FIND_TEXT'){
-        message.results = findText(message.find, message.results);
+        findText(message.find, message.results);
     }
 });
+
