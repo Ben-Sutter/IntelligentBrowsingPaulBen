f0b4ada5f9fff22917e7c705f2c2ba6add541775
Paul Kim <pauldk422@gmail.com>
2021-12-07 21:15:58 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -14,10 +14,26 @@ const searchText = document.getElementById("searchInput");
 const resultsText = document.getElementById("results");
 
 document.getElementById("searchButton").addEventListener("click", async () => {
+    resultsText.innerHTML = "";
     console.log("clicked");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        resultsText.innerHTML = "";
         console.log("clicked");
         chrome.tabs.sendMessage(tabs[0].id, {action: "FIND_TEXT", find: searchText.value, results: resultsText.value});
         // window.close();
+        chrome.storage.local.get("output", function(data) {
+          resultsText.innerHTML = "";
+          console.log(data.output)
+          if (data.output === "undefined") {
+            console.log("Clear")
+          } else {
+            resultsText.innerHTML = "";
+            resultsText.insertAdjacentHTML('afterbegin',"<ul>" + data.output + "</ul>");
+            chrome.storage.local.set({output: "undefined"});
+
+          }
+        });
+        chrome.storage.local.set({output: "undefined"});
+
     })
 });
